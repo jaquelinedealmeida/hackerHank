b0ab67899974de685540c0e2728d95bc31459ec9
Gustavo Caetano <gustavo.caetano13@gmail.com>
2022-02-13 12:19:16 -0300

    kangaroo

---
+++ algorithms-exercices/numberLineJumps.js
@@ -0,0 +1,17 @@
+function kangaroo(x1, v1, x2, v2) {
+  // Write your code here
+  let jump1 = x1;
+  let jump2 = x2;
+  let yesNo = 'NO';
+  for (let index = 0; index < 10000; index += 1) {
+    jump1 += v1;
+    jump2 += v2;
+    if (jump1 === jump2) {
+      yesNo = 'YES';
+    };
+  };
+  // console.log(yesNo);
+  return yesNo;
+};
+
+kangaroo(0, 3, 4, 2);
